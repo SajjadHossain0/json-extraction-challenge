f5585cd7eeee2fc56e7bb380edd6ad4d79b331d2
mdsaj <mdsajjadhossain614@gmail.com>
2025-03-30 15:16:15 +0600

    commit

---
+++ src/test/java/com/jsonextractionchallenge/JsonExtractionChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jsonextractionchallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsonExtractionChallengeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
